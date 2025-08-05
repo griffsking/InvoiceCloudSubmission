a9a09b564bcef630f1ba6867813c35e91d2f6514
griffsking <griffking0325@hotmail.com>
2025-08-05 13:29:48 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+
+export default function App() {
+  let loading = "Loading...";
+  fetch(
+    "https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/7a6561"
+  )
+    .then((response) => {
+      console.log("how many times this happening?");
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((text) => {
+      console.log(text);
+      document.getElementById("loading").remove();
+      for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+          const newDiv = document.createElement("li");
+          newDiv.textContent = text[i];
+          document.getElementById("list").appendChild(newDiv);
+        }, 500 * (i + 1));
+      }
+    })
+    .catch((error) => {
+      console.log(`Could not fetch: ${error}`);
+    });
+  return (
+    <div className="App">
+      <h1 id="loading">Loading...</h1>
+      <ul id="list"></ul>
+    </div>
+  );
+}
